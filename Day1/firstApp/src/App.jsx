afdcb520a7b48a0ee9ad5c8a643358dcf3ba85de
Aashis Kafle <158802926+aashis13@users.noreply.github.com>
2025-06-23 10:36:06 +0545

    first commit

---
+++ Day1/firstApp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <h1 className="text-red-500">This is my First React App!</h1>
+    <p>My name is Aashis Kafle</p>
+    </>
+  );
+}
+
+export default App;
